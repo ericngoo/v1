22555bfd36d978d63803d64d7e7707816b7b606a
Eric Ngo <engo1207@gmail.com>
2020-09-25 11:28:21 -0700

    Updates

---
+++ precache-manifest.89298be8d5961ec9783a9c2055e19e39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83b55b557cb221802e67bfda995be444",
+    "url": "/v1/index.html"
+  },
+  {
+    "revision": "19ab48a7513d52840fe6",
+    "url": "/v1/static/css/main.7f86d7ea.chunk.css"
+  },
+  {
+    "revision": "9fd0501bb29be0ee4b10",
+    "url": "/v1/static/js/2.65481413.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/v1/static/js/2.65481413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ab48a7513d52840fe6",
+    "url": "/v1/static/js/main.4eff9f1e.chunk.js"
+  },
+  {
+    "revision": "ae31ca608b8fd6bac8c7",
+    "url": "/v1/static/js/runtime-main.efb5f94e.js"
+  }
+]);
